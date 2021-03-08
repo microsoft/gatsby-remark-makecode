21aa22fcb086d4c788d0f1c207a01c9d33880828
pelikhan <jhalleux@microsoft.com>
2021-03-08 13:33:03 +0100

    added prettier config

--- makecode-headless.js
+++ makecode-headless.js
@@ -10,33 +10,45 @@ const path = require("path");
 
 const sharp = require("sharp");
 
+const {
+  promisify
+} = require("util");
+
+const fsExists = promisify(fs.existsSync);
 let initPromise;
 let browser;
 let page;
 let pendingRequests = {};
 let imagePath = ".cache/makecode";
-let versions;
+let targetInfo;
 
-const hash = req => crypto.createHash("md5").update(JSON.stringify(req) + versions).digest("hex");
+const hash = req => crypto.createHash("md5").update(JSON.stringify(req) + "|" + targetInfo).digest("hex");
 
 const cacheName = id => path.join(imagePath, id + ".png");
 
 exports.render = req => {
   const id = hash(req);
   const fn = cacheName(id);
-  if (fs.existsSync(fs)) req = JSON.parse(JSON.stringify(req));
-  req.type = "renderblocks";
-  req.id = id;
-  req.options = req.options || {};
-  return new Promise((resolve, reject) => {
-    pendingRequests[req.id] = {
-      req,
-      resolve
-    };
-    page.evaluate(async msg => {
-      const docs = document.getElementById("docs");
-      docs.contentWindow.postMessage(msg, "*");
-    }, req);
+  return fsExists(fn).then(efn => {
+    if (efn) {
+      console.debug(`mkcd: cache hit ${fn}`);
+      return fn;
+    }
+
+    req = JSON.parse(JSON.stringify(req));
+    req.type = "renderblocks";
+    req.id = id;
+    req.options = req.options || {};
+    return new Promise((resolve, reject) => {
+      pendingRequests[req.id] = {
+        req,
+        resolve
+      };
+      page.evaluate(async msg => {
+        const docs = document.getElementById("docs");
+        docs.contentWindow.postMessage(msg, "*");
+      }, req);
+    });
   });
 };
 
@@ -75,7 +87,7 @@ exports.init = options => initPromise || (initPromise = new Promise((resolve, re
         case "renderready":
           {
             console.info(`mkcd: renderer ready`);
-            versions = JSON.stringify(msg.versions);
+            targetInfo = options.url + "|" + JSON.stringify(msg.versions) + "|";
             pendingRequests = {};
             resolve();
             break;
@@ -87,10 +99,10 @@ exports.init = options => initPromise || (initPromise = new Promise((resolve, re
 
             const r = pendingRequests[id];
             if (!r) return;
-            delete pendingRequests[id];
-            console.info(`mkcd: rendered`, msg.id); // render to file 
+            delete pendingRequests[id]; // render to file
 
             const fn = saveReq(msg);
+            console.info(`mkcd: rendered`, fn);
             r.resolve(fn);
             break;
           }
--- src/makecode-headless.js
+++ src/makecode-headless.js
@@ -1,56 +1,57 @@
-const puppeteer = require("puppeteer")
-const crypto = require("crypto")
-const fs = require("fs")
-const path = require("path")
-const sharp = require("sharp")
-const { promisify } = require('util');
-const fsExists = promisify(fs.existsSync)
+const puppeteer = require("puppeteer");
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+const { promisify } = require("util");
+const fsExists = promisify(fs.existsSync);
 
-let initPromise
-let browser
-let page
-let pendingRequests = {}
-let imagePath = ".cache/makecode"
-let targetInfo
+let initPromise;
+let browser;
+let page;
+let pendingRequests = {};
+let imagePath = ".cache/makecode";
+let targetInfo;
 
-const hash = req =>
-    crypto.createHash("md5").update(JSON.stringify(req) + "|" + targetInfo).digest("hex")
+const hash = (req) =>
+    crypto
+        .createHash("md5")
+        .update(JSON.stringify(req) + "|" + targetInfo)
+        .digest("hex");
 
-const cacheName = (id) => path.join(imagePath, id + ".png")
+const cacheName = (id) => path.join(imagePath, id + ".png");
 
-exports.render = req => {
-    const id = hash(req)
-    const fn = cacheName(id)
-    const efn = await fsExists(fn)
-    if (efn) {
-        console.debug(`mkcd: cache hit ${fn}`)
-        return fn;
-    }
-
-    req = JSON.parse(JSON.stringify(req))
-    req.type = "renderblocks"
-    req.id = id
-    req.options = req.options || {}
-    return new Promise((resolve, reject) => {
-        pendingRequests[req.id] = {
-            req,
-            resolve,
+exports.render = (req) => {
+    const id = hash(req);
+    const fn = cacheName(id);
+    return fsExists(fn).then((efn) => {
+        if (efn) {
+            console.debug(`mkcd: cache hit ${fn}`);
+            return fn;
         }
-        page.evaluate(async msg => {
-            const docs = document.getElementById("docs")
-            docs.contentWindow.postMessage(msg, "*")
-        }, req)
-    })
-}
+        req = JSON.parse(JSON.stringify(req));
+        req.type = "renderblocks";
+        req.id = id;
+        req.options = req.options || {};
+        return new Promise((resolve, reject) => {
+            pendingRequests[req.id] = {
+                req,
+                resolve,
+            };
+            page.evaluate(async (msg) => {
+                const docs = document.getElementById("docs");
+                docs.contentWindow.postMessage(msg, "*");
+            }, req);
+        });
+    });
+};
 
-const saveReq = async msg => {
+const saveReq = async (msg) => {
     // id is the hash of the request
     const fpng = path.join(imagePath, msg.id + ".png");
-    await sharp(msg.uri)
-        .png()
-        .toFile(fpng)
+    await sharp(msg.uri).png().toFile(fpng);
     return fpng;
-}
+};
 
 /**
  * Loads the iframe in headless chrome
@@ -62,44 +63,48 @@ const saveReq = async msg => {
  *   }
  */
 
-exports.init = options =>
+exports.init = (options) =>
     initPromise ||
     (initPromise = new Promise((resolve, reject) => {
-        console.info(`mkcd: initializing ${options.url}`)
-        ;(async () => {
-            console.info(`mkcd: storing images in ${imagePath}`)
+        console.info(`mkcd: initializing ${options.url}`);
+        (async () => {
+            console.info(`mkcd: storing images in ${imagePath}`);
             if (!fs.existsSync(imagePath))
-                fs.mkdirSync(imagePath, { recursive: true })
-            browser = await puppeteer.launch()
-            page = await browser.newPage()
-            page.on("console", msg => console.log(msg.text()))
-            await page.exposeFunction("ssrPostMessage", msg => {
-                if (msg.source != "makecode") return
+                fs.mkdirSync(imagePath, { recursive: true });
+            browser = await puppeteer.launch();
+            page = await browser.newPage();
+            page.on("console", (msg) => console.log(msg.text()));
+            await page.exposeFunction("ssrPostMessage", (msg) => {
+                if (msg.source != "makecode") return;
 
                 switch (msg.type) {
                     case "renderready": {
-                        console.info(`mkcd: renderer ready`)
-                        targetInfo = options.url + '|' + JSON.stringify(msg.versions) + '|'
-                        pendingRequests = {}
-                        resolve()
-                        break
+                        console.info(`mkcd: renderer ready`);
+                        targetInfo =
+                            options.url +
+                            "|" +
+                            JSON.stringify(msg.versions) +
+                            "|";
+                        pendingRequests = {};
+                        resolve();
+                        break;
                     }
 
                     case "renderblocks": {
-                        const id = msg.id // this is the id you sent
+                        const id = msg.id; // this is the id you sent
 
-                        const r = pendingRequests[id]
-                        if (!r) return
-                        delete pendingRequests[id]
-                        // render to file 
-                        const fn = saveReq(msg)
-                        console.info(`mkcd: rendered`, fn)
-                        r.resolve(fn)
-                        break
+                        const r = pendingRequests[id];
+                        if (!r) return;
+                        delete pendingRequests[id];
+                        // render to file
+                        const fn = saveReq(msg);
+                        console.info(`mkcd: rendered`, fn);
+                        r.resolve(fn);
+                        break;
                     }
                 }
-            })
-            const rendererUrl = `${options.url}---docs?render=1`
+            });
+            const rendererUrl = `${options.url}---docs?render=1`;
             const html = `<body>
       <iframe id="docs" src=""></iframe>
       <script>
@@ -107,8 +112,8 @@ exports.init = options =>
           const docs = document.getElementById("docs")
           docs.src="${rendererUrl}"
       </script>
-      </body>`
-            console.info(`mkcd: loading ${rendererUrl}`)
-            await page.setContent(html)
-        })()
-    }))
+      </body>`;
+            console.info(`mkcd: loading ${rendererUrl}`);
+            await page.setContent(html);
+        })();
+    }));
