7c58b6ea0e7bff812553df67f50051e7cbf71828
pelikhan <jhalleux@microsoft.com>
2021-09-14 14:46:27 -0700

    support for custom editor url

--- src/index.ts
+++ src/index.ts
@@ -3,87 +3,68 @@ const makecode = require("./makecode-headless");
 
 const validLanguages = [`blocks`];
 
-/*
-const remark = require("remark") 
-{
-const tree = remark().parse("![Caption](./imageurl.png)")
-visit(tree, "image", console.log)
-}
-{
-  const tree = remark().parse(
-`
-\`\`\`blocks
-let x = 0
-\`\`\`
-`
-
-  )
-  visit(tree, "code", console.log)
-  }
-*/
-module.exports = async ({
-  markdownAST
-}, pluginOptions = {}) => {
-  const url = "https://makecode.microbit.org/beta";
-  await makecode.init({
-    url,
-    cache: "./public/images/makecode"
-  });
-/*
+module.exports = async (
+    { markdownAST },
+    pluginOptions: { editorUrl?: string } = {}
+) => {
+    const url = pluginOptions?.editorUrl || "https://makecode.microbit.org/";
+    await makecode.init({
+        url,
+        cache: "./public/images/makecode",
+    });
+    /*
   {
     type: 'code',
     lang: 'blocks',
     value: 'let x = 0'
   }
-*/  
-  let codeNodes = [];
-  visit(markdownAST, `code`, node => {
-    const chunks = (node.lang || ``).match(/^(\S+)(\s+(.+))?/);
+*/
+    const codeNodes = [];
+    visit(markdownAST, `code`, (node) => {
+        const chunks = (node.lang || ``).match(/^(\S+)(\s+(.+))?/);
 
-    if (!chunks || !chunks.length) {
-      return node;
-    }
-
-    const lang = chunks[1];
-    const attrString = chunks[3];
-    if (validLanguages.includes(lang)) {
-      node.lang = lang;
-      codeNodes.push({
-        node,
-        attrString: attrString
-      });
-    }
-
-    return node;
-  });
-  await Promise.all(codeNodes.map(async ({
-    node,
-    attrString
-  }) => {
-    const {
-      value,
-      lang
-    } = node;
-
-    try {
-      const rendered = await makecode.render({
-        code: value,
-        options: {
-          pixelDensity: 1
+        if (!chunks || !chunks.length) {
+            return node;
         }
-      });
-      // store rendered in node
-      node.value = JSON.stringify({
-        source: node.value,
-        rendered
-      })
-      //node.html = `html`;
-      //node.value = `<div class="makecode"><img class="blocks" src="${url}" height=${height}" alt="MakeCode code snippet" loading="lazy" /></div>`
-    } catch (error) {
-      console.log(`Error during makecode execution. Leaving code block unchanged`);
-      console.log(error);
-    }
 
-    return node;
-  }));
+        const lang = chunks[1];
+        const attrString = chunks[3];
+        if (validLanguages.includes(lang)) {
+            node.lang = lang;
+            codeNodes.push({
+                node,
+                attrString: attrString,
+            });
+        }
+
+        return node;
+    });
+    await Promise.all(
+        codeNodes.map(async ({ node, attrString }) => {
+            const { value, lang } = node;
+
+            try {
+                const rendered = await makecode.render({
+                    code: value,
+                    options: {
+                        pixelDensity: 1,
+                    },
+                });
+                // store rendered in node
+                node.value = JSON.stringify({
+                    source: node.value,
+                    rendered,
+                });
+                //node.html = `html`;
+                //node.value = `<div class="makecode"><img class="blocks" src="${url}" height=${height}" alt="MakeCode code snippet" loading="lazy" /></div>`
+            } catch (error) {
+                console.log(
+                    `Error during makecode execution. Leaving code block unchanged`
+                );
+                console.log(error);
+            }
+
+            return node;
+        })
+    );
 };
